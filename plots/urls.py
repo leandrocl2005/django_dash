e19c1b4f167c795eb4000c332eb5c47292ff949a
leandrocl2005 <leandrocl2005@yahoo.com.br>
2018-09-30 11:24:26 -0300

    logout done

--- plots/urls.py
+++ plots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, logout_view
 
 urlpatterns = [
-	path('',index,name='index'),
+	path('', index, name='index'),
+	path('logout', logout_view, name='logout')
 ]
--- plots/views.py
+++ plots/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
 @login_required(login_url='/login/')
 def index(request):
 	return render(request,'index.html')
+
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+    return render(request,'login.html')
